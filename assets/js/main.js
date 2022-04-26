47f7055f1d62970ceb320535afda616ac30a5cf3
Eberthyn <eberthyn@gmail.com>
2022-04-26 15:51:45 -0300

    Capa do Curso

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+/* Fixa Menu Superior */
+$(window).scroll(function () {
+
+  if ($(window).scrollTop() + $(window).height() === $(document).height()) {
+
+  } else {
+    $(".navbar").addClass("fixed-top");
+  }
+
+  if ($(window).scrollTop() === 0) {
+    $(".navbar").removeClass("fixed-top");
+    $("#row-main").css("padding-top", "0%");
+    $(".navbar").css("box-shadow", "0px 0px 0px rgba(0,0,0,0.5)")
+
+  } else {
+    $(".navbar").addClass("fixed-top");
+    $("#row-main").css("padding-top", ($('#menuSuperior').css("height")));
+    $(".stepContainer").css("padding-bottom", ($('#menuSuperior').css("height")));
+    $(".navbar").css("box-shadow", "0px 2px 2px rgba(0,0,0,0.5)")
+  }
+
+});
+
+/*Controla funções no Menu Lateral*/
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+
+    var links = this.el.find('.link');
+
+    links.on('click', { el: this.el, multiple: this.multiple }, this.dropdown);
+  }
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el;
+    $this = $(this),
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+    };
+
+
+    if ($(".step:last-child .contentMenu").hasClass("open")) {
+      $(".step:last-child .line").show();
+    } else {
+      $(".step:last-child .line").hide();
+    }
+  }
+
+  /* Mostra linha da última Aula */
+  var accordion = new Accordion($('#accordion'), false);
+  if ($(".step:last-child .contentMenu").hasClass("open")) {
+    $(".step:last-child .line").show();
+  } else {
+    $(".step:last-child .line").hide();
+  }
+
+});
+
+/* Deixa a aula atual aberta */
+$(".contentMenu.open .submenu").show();
+$(".step:last-child .line").hide();
+
+/* Adiciona suavidade na rolagem da ancoragem */
+$(document).on("click", 'a[href^="#"]', function (e) {
+  e.preventDefault();
+  var target = $(this).attr("href"); //Get the target
+  var scrollToPosition = $(target).offset().top;
+
+  $('html,body').animate({ scrollTop: scrollToPosition - 150 }, 600);
+});
+
+/* Adiciona tooltip (Abas) */
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+/* Controlador do Zoom*/
+var currFFZoom = 1;
+var currIEZoom = 100;
+
+function plus() {
+  //alert('sad');
+  var step = 0.05;
+  currFFZoom += step;
+  $('body').css('MozTransform', 'scale(' + currFFZoom + ')');
+  var stepie = 5;
+  currIEZoom += stepie;
+  $('body').css('zoom', ' ' + currIEZoom + '%');
+
+};
+function minus() {
+  //alert('sad');
+  var step = 0.05;
+  currFFZoom -= step;
+  $('body').css('MozTransform', 'scale(' + currFFZoom + ')');
+  var stepie = 5;
+  currIEZoom -= stepie;
+  $('body').css('zoom', ' ' + currIEZoom + '%');
+};
+
+function restore() {
+  currFFZoom = 100;
+  $('body').css('MozTransform', 'scale(' + currFFZoom + ')');
+  currIEZoom = 100;
+  $('body').css('zoom', ' ' + currIEZoom + '%');
+};
