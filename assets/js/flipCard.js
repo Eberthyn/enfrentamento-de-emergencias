03556e1ba9fcb8f7eef1459719c783269203880d
Eberthyn <eberthyn@gmail.com>
2022-05-13 06:20:19 -0300

    Flip Cards - Aula 9

---
+++ assets/js/flipCard.js
@@ -0,0 +1,14 @@
+/*Flip Card*/
+  
+  function flip() {
+    $('#quiz').toggleClass('flipped');
+  };
+
+  function flip2() {
+    $('#quiz2').toggleClass('flipped');
+  };
+  
+  function flip3() {
+    $('#quiz3').toggleClass('flipped');
+  };
+  
